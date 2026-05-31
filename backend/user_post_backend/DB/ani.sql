151e44826b558d6b6bb097ab6c079d754c3da2c3
CheriaAshley <3406872918@qq.com>
2026-05-31 15:18:38 +0800

    修复bug

--- backend/user_post_backend/DB/ani.sql
+++ backend/user_post_backend/DB/ani.sql
@@ -4,7 +4,7 @@ DEFAULT COLLATE utf8mb4_general_ci;
 
 USE anime_web;
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     user_name VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE user (
     create_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE anime_post (
+CREATE TABLE IF NOT EXISTS anime_post (
     ani_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     ani_name VARCHAR(100) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE anime_post (
     UNIQUE(user_id, ani_name)
 );
 
-CREATE TABLE character_post (
+CREATE TABLE IF NOT EXISTS character_post (
     char_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     char_name VARCHAR(100) NOT NULL,
@@ -38,7 +38,7 @@ CREATE TABLE character_post (
     FOREIGN KEY (user_id) REFERENCES user(user_id),
     UNIQUE(user_id, char_name)
 );
-CREATE TABLE anime_favorite (
+CREATE TABLE IF NOT EXISTS anime_favorite (
     user_id INT NOT NULL,
     ani_id INT NOT NULL,
     create_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -49,7 +49,7 @@ CREATE TABLE anime_favorite (
     FOREIGN KEY (ani_id) REFERENCES anime_post(ani_id)
 );
 
-CREATE TABLE character_favorite (
+CREATE TABLE IF NOT EXISTS character_favorite (
     user_id INT NOT NULL,
     char_id INT NOT NULL,
     create_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -59,22 +59,7 @@ CREATE TABLE character_favorite (
     FOREIGN KEY (user_id) REFERENCES user(user_id),
     FOREIGN KEY (char_id) REFERENCES character_post(char_id)
 );
-INSERT INTO user(user_name, password, photo, user_intro)
-VALUES
-('Cheria', '123456', 'upload/me.jpg', '喜欢日常系和轻百合'),
-('小明', '123456', 'upload/a.jpg', '喜欢热血番');
 
-INSERT INTO anime_post(user_id, ani_name, ani_img, ani_type, ani_com)
-VALUES
-(1, '孤独摇滚', 'upload/bocchi.jpg', '校园,音乐,日常', '波奇酱太真实了'),
-(2, '孤独摇滚', 'upload/bocchi2.jpg', '校园,音乐,日常', '我也很喜欢这部番'),
-(1, '轻音少女', 'upload/k-on.jpg', '音乐,日常', '很温暖的日常番');
-
-INSERT INTO character_post(user_id, char_name, char_from, char_img, char_com)
-VALUES
-(1, '后藤一里', '孤独摇滚', 'upload/bocchi_role.jpg', '社恐人圣经'),
-(2, '后藤一里', '孤独摇滚', 'upload/bocchi_role2.jpg', '很可爱'),
-(1, '平泽唯', '轻音少女', 'upload/yui.jpg', '天然呆很可爱');
 SELECT 
     a.ani_id,
     a.user_id,
