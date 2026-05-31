63a5caaf103957e092b55f4ff31c8e8871a452ea
CheriaAshley <3406872918@qq.com>
2026-05-31 15:48:22 +0800

    bug

--- backend/user_post_backend/app.py
+++ backend/user_post_backend/app.py
@@ -1060,4 +1060,4 @@ def favorite_character_list(user_id):
             conn.close()
             
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
